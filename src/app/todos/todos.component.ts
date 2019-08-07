5632d42ee1f16e72f8dcc85ae93bdda3173152d2
Art Schedrov <art.worknsk@gmail.com>
2019-08-08 00:02:50 +0700

    learn ang 08.08

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {TodosService} from '../shared/todos.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  constructor(private todosService: TodosService) {}
+
+  ngOnInit() {
+  }
+
+  onChange(id: number) {
+    this.todosService.onToggle(id);
+  }
+}
